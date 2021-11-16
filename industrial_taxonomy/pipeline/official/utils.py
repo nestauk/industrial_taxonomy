1e030b7d5cac038ad9bbdcb5153a2df4656c468b
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2021-11-16 16:20:59 +0000

    added script with utilities to fetch official data

---
+++ industrial_taxonomy/pipeline/official/utils.py
@@ -0,0 +1,23 @@
+"""Get and extract official data
+"""
+
+import requests
+import pandas as pd
+
+
+def get(url: str) -> bytes:
+    """get ONS table in URL"""
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
+
+
+def excel_to_df(content: bytes, sheets: int, skiprows: int):
+    """Parse excel to dataframe
+    Arguments:
+        sheets: sheet number to keep
+        skipwrows: rows to skip
+    """
+    return pd.read_excel(content, skiprows=skiprows, sheet_name=sheets).dropna(
+        axis=0, subset=["LA code"]  # We are dropping bottom rows without a LA code
+    )
