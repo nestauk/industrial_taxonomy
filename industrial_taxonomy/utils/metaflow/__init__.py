7d02d981936df7943ad62599b0ae5896a84569d6
bishax <alex.bishop@nesta.org.uk>
2021-11-23 17:39:41 +0000

    fixup! Add Metaflow namespace context manager utility

--- industrial_taxonomy/utils/metaflow/__init__.py
+++ industrial_taxonomy/utils/metaflow/__init__.py
@@ -1,1 +1,3 @@
 from .client import namespace_context
+
+__all__ = ["namespace_context"]
--- industrial_taxonomy/utils/metaflow/client.py
+++ industrial_taxonomy/utils/metaflow/client.py
@@ -3,12 +3,13 @@
 https://docs.metaflow.org/metaflow/client
 """
 from contextlib import contextmanager
+from typing import Generator, Optional
 
 from metaflow import get_namespace, namespace
 
 
 @contextmanager
-def namespace_context(ns: str) -> str:
+def namespace_context(ns: Optional[str]) -> Generator[Optional[str], None, None]:
     """Context manager to temporarily enter metaflow namespace `ns`."""
     old_ns = get_namespace()
     namespace(ns)
