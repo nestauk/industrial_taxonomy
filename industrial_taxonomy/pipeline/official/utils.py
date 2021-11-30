23b27905d4d44023313fd1bdada0e421b86779ba
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2021-11-30 15:33:42 +0000

    Removed the excel_to_df function

--- industrial_taxonomy/pipeline/official/utils.py
+++ industrial_taxonomy/pipeline/official/utils.py
@@ -10,12 +10,3 @@ def get(url: str) -> bytes:
     response = requests.get(url)
     response.raise_for_status()
     return response.content
-
-
-def excel_to_df(content: bytes, sheets: int, skiprows: int):
-    """Parse excel to dataframe
-    Arguments:
-        sheets: sheet number to keep
-        skipwrows: rows to skip
-    """
-    return pd.read_excel(content, skiprows=skiprows, sheet_name=sheets)
