98a10bbdb076e796381fe07ad8bb23d4cbae7902
bishax <alex.bishop@nesta.org.uk>
2021-12-07 13:23:00 +0000

    fixup! Jacchammer flow

--- industrial_taxonomy/pipeline/glass_house/flow.py
+++ industrial_taxonomy/pipeline/glass_house/flow.py
@@ -22,6 +22,9 @@ except ImportError:
     pass
 
 
+CHUNKSIZE = 100_000  # Chunksize of matches output by Jacchammer and persisted to S3
+
+
 @conda_base(python="3.9")  # Need >=3.8 on Batch for TypedDict of FullMatchResult
 @project(name="industrial_taxonomy")
 class JacchammerFlow(FlowSpec):
@@ -29,12 +32,23 @@ class JacchammerFlow(FlowSpec):
 
     Attributes:
         run_id: The origin run ID of the flow
-        names_x: Companies House company names
-        names_y: Glass AI organisation names
-        clean_names_x: Pre-processed Companies House company names
-        clean_names_y: Pre-processed Glass AI organisation names
+        ch_names: Companies House company names
+        glass_names: Glass AI organisation names
+        clean_ch_names: Pre-processed Companies House company names
+        clean_glass_names: Pre-processed Glass AI organisation names
         full_top_matches: Full match results
     """
+
+    drop_matches_below = Parameter(
+        "drop-matches-below",
+        help=(
+            "Internal optimisation to reduce number of matches persisted."
+            "Should still be low enough to make a domain decision about what "
+            "score constitutes a good match."
+        ),
+        type=int,
+        default=50,
+    )
 
     test_mode = Parameter(
         "test-mode",
@@ -49,29 +63,24 @@ class JacchammerFlow(FlowSpec):
         default=False,
     )
 
-    names_x: "DataFrame"
-    names_y: "DataFrame"
-    clean_names_x: "DataFrame"
-    clean_names_y: "DataFrame"
+    ch_names: "DataFrame"
+    glass_names: "DataFrame"
+    clean_ch_names: "DataFrame"
+    clean_glass_names: "DataFrame"
     run_id: int
     full_top_matches: List[FullMatchResult]  # TODO: py37 error
 
     @step
     def start(self):
-        """Load raw data"""
-        from industrial_taxonomy.getters.companies_house import (
-            get_name as get_companies_house_names,
-        )
-        from industrial_taxonomy.getters.glass import (
-            get_organisation as get_glass_organisation,
-        )
+        """Load raw data."""
+        from industrial_taxonomy.getters import glass, companies_house
 
         self.run_id = current.origin_run_id or current.run_id
 
         nrows = 10_000 if (self.test_mode and not current.is_production) else None
 
-        self.names_x = get_companies_house_names().name.head(nrows)
-        self.names_y = get_glass_organisation().name.head(nrows)
+        self.ch_names = companies_house.get_name().name.head(nrows)
+        self.glass_names = glass.get_organisation().name.head(nrows)
 
         self.next(self.clean_names)
 
@@ -81,11 +90,13 @@ class JacchammerFlow(FlowSpec):
         """Pre-process names if `self.should_clean_names`."""
         from jacc_hammer.name_clean import preproc_names
 
-        self.clean_names_x = (
-            preproc_names(self.names_x) if self.should_clean_names else self.names_x
+        self.clean_ch_names = (
+            preproc_names(self.ch_names) if self.should_clean_names else self.ch_names
         )
-        self.clean_names_y = (
-            preproc_names(self.names_y) if self.should_clean_names else self.names_y
+        self.clean_glass_names = (
+            preproc_names(self.glass_names)
+            if self.should_clean_names
+            else self.glass_names
         )
 
         self.next(self.match)
@@ -94,75 +105,39 @@ class JacchammerFlow(FlowSpec):
     @pip(path="requirements.txt")
     @step
     def match(self):
-        """The core fuzzy matching algorithm"""
-        from itertools import starmap as starmap_
+        """The core fuzzy matching algorithm."""
         from pathlib import Path
         from tempfile import TemporaryDirectory
 
-        from jacc_hammer.fuzzy_hash import (
-            Cos_config,
-            Fuzzy_config,
-            match_names_stream,
-        )
-        from pandas import DataFrame
-        from toolz.curried import curry, pipe
-        from toolz.curried import map as tmap
+        from jacc_hammer.fuzzy_hash import match_names_stream
 
         from utils import optimise_chunk
-
-        starmap = curry(starmap_)
-
-        def persist_chunk(chunk_id: int, chunk: DataFrame) -> int:
-            key = f"match-chunks-{chunk_id}"
-            with S3(run=self) as s3:
-                s3.put(key, chunk.to_csv())
-            print(f"Persisted chunk {chunk_id} of shape {chunk.shape}")
-            return chunk_id
 
         tmp_dir = Path(TemporaryDirectory(dir=".").name)
         tmp_dir.mkdir()
 
-        cos_config = Cos_config()
-        fuzzy_config = Fuzzy_config(num_perm=128)
-        match_config = dict(
-            threshold=50,
-            chunksize=100_000,
-            cos_config=cos_config,
-            fuzzy_config=fuzzy_config,
+        chunks = match_names_stream(
+            [self.clean_ch_names.tolist(), self.clean_glass_names.tolist()],
+            threshold=self.drop_matches_below,
+            chunksize=CHUNKSIZE,
             tmp_dir=tmp_dir,
         )
+        self._chunk_ids = [
+            self._persist_chunk(id, optimise_chunk(chunk))
+            for id, chunk in enumerate(chunks)
+        ]
 
-        self._chunk_ids: List[int] = pipe(
-            match_names_stream(
-                [self.clean_names_x.tolist(), self.clean_names_y.tolist()],
-                **match_config,
-            ),
-            tmap(optimise_chunk),
-            enumerate,  # Give chunks ID's
-            starmap(persist_chunk),
-            list,  # Realise computation
-        )
         self.next(self.extract_top_matches)
 
     @resources(memory=64_000)
     @step
     def extract_top_matches(self):
         """Extract top matches."""
-        from io import StringIO
-        from pandas import concat, DataFrame, read_csv
+        from pandas import concat
         from utils import get_top_matches
 
-        def get_chunk(chunk_id: int) -> DataFrame:
-            key = f"match-chunks-{chunk_id}"
-            try:
-                with S3(run=self) as s3:
-                    return read_csv(StringIO(s3.get(key).text))
-            except MetaflowS3NotFound:  # In case of Flow resumption
-                with S3(run=Run(f"{current.flow_name}/{current.origin_run_id}")) as s3:
-                    return read_csv(StringIO(s3.get(key).text))
-
         self.top_matches = get_top_matches(
-            concat(map(get_chunk, self._chunk_ids)).reset_index(drop=True)
+            concat(map(self._get_chunk, self._chunk_ids)).reset_index(drop=True)
         )
 
         print("Number of matches: ", self.top_matches.shape[0])
@@ -174,22 +149,40 @@ class JacchammerFlow(FlowSpec):
 
         self.full_top_matches = (
             self.top_matches.merge(
-                self.names_y.reset_index().rename(columns={"index": "index_y"}),
+                self.glass_names.reset_index().rename(columns={"name": "org_name"}),
                 left_on="y",
                 right_index=True,
                 validate="1:1",
             )
             .merge(
-                self.names_x.reset_index().rename(columns={"index": "index_x"}),
+                self.ch_names.reset_index().rename(columns={"name": "company_name"}),
                 left_on="x",
                 right_index=True,
                 validate="m:1",
             )
-            .rename(columns={"name_x": "company_name", "name_y": "org_name"})
             .drop(["x", "y"], axis=1)
             .to_dict("records")
         )
         del self.top_matches
+
+    def _persist_chunk(self, chunk_id: int, chunk: "DataFrame") -> int:
+        key = f"match-chunks-{chunk_id}"
+        with S3(run=self) as s3:
+            s3.put(key, chunk.to_csv())
+        print(f"Persisted chunk {chunk_id} of shape {chunk.shape}")
+        return chunk_id
+
+    def _get_chunk(self, chunk_id: int) -> "DataFrame":
+        from io import StringIO
+        from pandas import read_csv
+
+        key = f"match-chunks-{chunk_id}"
+        try:
+            with S3(run=self) as s3:
+                return read_csv(StringIO(s3.get(key).text))
+        except MetaflowS3NotFound:  # If flow resumed, could be under `origin_run_id`
+            with S3(run=Run(f"{current.flow_name}/{current.origin_run_id}")) as s3:
+                return read_csv(StringIO(s3.get(key).text))
 
 
 if __name__ == "__main__":
