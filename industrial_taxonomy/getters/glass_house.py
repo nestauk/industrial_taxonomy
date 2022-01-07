84229e8a67d5d83f3cacf525cb76d54e38a2cb25
George Richardson <georgerichardson@posteo.net>
2022-01-07 17:48:57 +0000

    move getters to glass_house

--- industrial_taxonomy/getters/glass_house.py
+++ industrial_taxonomy/getters/glass_house.py
@@ -2,6 +2,7 @@
 from typing import Dict, List, Optional, TypedDict
 
 from metaflow import Run
+import numpy.typing as npt
 
 from industrial_taxonomy import config
 from industrial_taxonomy.utils.metaflow import get_run
@@ -59,3 +60,23 @@ def get_description_tokens(run: Optional[Run] = None) -> Dict[str, List[str]]:
     """Processed Glass description tokens for which a Companies House match exists."""
     run = run or get_run("GlassNlpFlow")
     return run.data.documents
+
+
+def description_embeddings(
+    run: Optional[Run] = None,
+) -> npt.ArrayLike:
+    """Gets embeddings of Glass organisation descriptions.
+
+    Returns a 2d array of size (n, m) where n is the number of companies and
+    m is the length of each embedding.
+    """
+    run = run or get_run("GlassEmbed")
+    return run.data.embeddings
+
+
+def embedding_org_ids(run: Optional[Run] = None) -> List[int]:
+    """Gets IDs of embedded Glass organisations."""
+    # run = run or get_run()
+    run = run or get_run("GlassEmbed")
+    # step = Step(f"GlassEmbed/{run.id}/start")
+    return run.data.org_ids
