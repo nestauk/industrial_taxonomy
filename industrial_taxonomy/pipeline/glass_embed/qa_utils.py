a7f05f14ffae9878598328c511a745d91e1e69a4
George Richardson <georgerichardson@posteo.net>
2022-01-12 17:48:04 +0000

    add util for random selection

--- industrial_taxonomy/pipeline/glass_embed/qa_utils.py
+++ industrial_taxonomy/pipeline/glass_embed/qa_utils.py
@@ -2,20 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def full_text_tokenize(texts, tokenizer):
-    """Tokenize texts without truncation"""
-    tokenized = tokenizer(texts, padding=True, truncation=False, return_tensors="pt")
-    return tokenized
+def _tokenize_full_text(texts, tokenizer):
+    """Tokenize texts without truncation."""
+    return tokenizer(texts, padding=True, truncation=False, return_tensors="np")
 
 
-def tokenized_original_length(tokenized):
+def token_count(texts, tokenizer):
     """Get number of tokens in original text after tokenization."""
+    tokenized = _tokenize_full_text(texts, tokenizer)
     return np.array(tokenized["attention_mask"].sum(axis=1))
 
 
 def tokenized_length_histogram(lengths, tokenizer):
     """Plots a cumulative histogram of tokenized text lengths with vertical
     line indicating `model_max_length`.
+
+    Args:
+        lengths: List of lengths (number of tokens) of tokenized texts
+        tokenizer: A `transformers` tokenizer
+
+    Returns:
+        fig: Cumulative normalised histogram of document lengths with a
+            vertical line indicating the max tokens of the model
+            associated with `tokenizer`.
     """
     max_tokens = np.max(lengths)
     model_max_length = tokenizer.model_max_length
@@ -29,8 +38,16 @@ def tokenized_length_histogram(lengths, tokenizer):
         bins=max_tokens,
     )
     ax.axvline(model_max_length, color="gray", linestyle="--")
-    ax.set_xlim(0, max_tokens)
     ax.set_xlabel("N Tokens")
+    ax.set_xlim(0, max_tokens)
     ax.set_ylabel("Cumulative Frequency (norm)")
 
     return fig
+
+
+def random_sample_idx(x, n_samples):
+    """Get a random sample of elements from a list-like without replacement."""
+    n_total = len(x)
+    rn_gen = np.random.default_rng()
+    sample_idx = rn_gen.choice(n_total, size=n_samples, replace=False)
+    return sample_idx
