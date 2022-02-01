4e307b66958040a8e3c6200aa74170687d8c1c52
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-02-01 12:47:37 +0000

    addressed pr review comments

--- industrial_taxonomy/getters/text_sectors.py
+++ industrial_taxonomy/getters/text_sectors.py
@@ -15,16 +15,24 @@ glass_id = str
 def text_sectors(
     run: Optional[Run] = None,
 ) -> Dict[clustering_param, List[Tuple[sector_id, glass_id]]]:
-    """Gets text sector assignments from the topic modelling"""
+    """Gets text sector assignments from the topic modelling
 
+    Args:
+        run: what ClusterGlass flow run to use.
+            Defaults to the latest production run
+
+    Returns:
+        A lookup between cluster assignment parameters (how many
+        companies do we tell topsbm to assign to each cluster based on
+        their representativeness) and the actual cluster assignments
+        (a tuple with sector id and company id)
+
+    """
     run = run or get_run("ClusterGlass")
-
     return run.data.clusters
 
 
 def topsbm_models(run: Optional[Run] = None) -> Dict[sector_id, sbmtm]:
     """Gets topic models trained on each sector"""
-
     run = run or get_run("ClusterGlass")
-
     return run.data.models
