bfbfc73792a725488c137d73e6d7583a8a12761d
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-02-15 16:52:45 +0000

    added functions to calculate silh scores to utils

--- industrial_taxonomy/pipeline/glass_clusters/utils.py
+++ industrial_taxonomy/pipeline/glass_clusters/utils.py
@@ -1,8 +1,13 @@
-"""Utils to create a sector corpus"""
+"""Utils to create a sector corpus
+and implement diagnostics of clustering homogeneity"""
 from toolz import pipe
 from functools import partial
-from typing import Dict, List
+from typing import Dict, List, Union, Tuple
 import pandas as pd
+import numpy as np
+from sklearn.metrics import silhouette_samples, silhouette_score
+from industrial_taxonomy.pipeline.glass_clusters.sbmtm import sbmtm
+from industrial_taxonomy.pipeline.glass_clusters.topic_utils import extract_topic_mix
 
 NE_CODES = {
     "CARDINAL",
@@ -99,3 +104,111 @@ def make_sector_corpora(
             big_sector_tokens_lookup, gl_sic4=glass_sic4, big_sectors=selected_sectors
         ),
     )
+
+
+def get_sector_companies(
+    text_sectors: List[Tuple[str, str]], sic4: str
+) -> Dict[str, str]:
+    """Creates lookup between company ids and text sectors inside a sic4
+
+    Args:
+        text_sectors: company ids and their text sectors
+        sic4: sic code
+
+    Returns:
+        Lookup between company id and text sectors within a sic4
+    """
+
+    return {
+        assign[1]: assign[0] for assign in filter(lambda x: sic4 in x[0], text_sectors)
+    }
+
+
+def get_topics_labels(
+    sector_companies: Dict[str, str], topic_mix: pd.DataFrame
+) -> List[np.array]:
+    """Get topic mix and sector labels for the silhouette analysis"""
+
+    tm_filt = topic_mix.assign(
+        sector=lambda df: df.index.map(sector_companies)
+    ).dropna()
+
+    tm_filt_x = tm_filt[topic_mix.columns].to_numpy()
+    tm_filt_labs = tm_filt["sector"].to_numpy()
+
+    return [tm_filt_x, tm_filt_labs]
+
+
+def get_silouhette_scores(
+    x_labs: List[np.array], dist_metric: str = "cosine"
+) -> pd.DataFrame:
+    """Calculate aggregate and cluster-level silouhette scores
+
+    Args:
+        x_labs: sector label and topic mix for each company
+        dist_metric: distance measure to calculate the silhouette score
+
+    Returns:
+        sic level and text sector level silhouette scores
+
+    """
+
+    X, labs = x_labs
+
+    s_score = silhouette_score(X, labs, metric=dist_metric)
+
+    s_cluster = (
+        pd.DataFrame(
+            {"score": silhouette_samples(X, labs, metric=dist_metric), "cluster": labs}
+        )
+        .groupby("cluster")
+        .mean()
+    )
+
+    return {"local_score": s_score, "cluster_scores": s_cluster}
+
+
+def text_sector_silhouette(
+    companies_sector_all: List[Tuple[str, str]],
+    sector: str,
+    models: Dict[str, sbmtm],
+) -> Dict[str, Union[float, pd.DataFrame]]:
+    """Return silouhette scores for companies in a sector
+
+    Args:
+        companies_sector_all: companies with text sector labels
+        models: tobsbm models
+        sector: focal sector
+
+    Returns:
+        Sector (SIC) and silhouette scores
+    """
+
+    return pipe(
+        companies_sector_all,
+        partial(get_sector_companies, sic4=sector),
+        partial(get_topics_labels, topic_mix=extract_topic_mix(models[sector])),
+        get_silouhette_scores,
+    )
+
+
+def filter_clusters(clusters_silh_scores: pd.DataFrame, q: float) -> dict:
+    """Filters clusters based on the position of their silhouette score in the
+        distribution
+
+    Args:
+        clusters_silh_scores: cluster level silhouette scores
+        q: silhouette threshold
+
+    Returns:
+        Lookup between assignment strategies and filtered sectors
+    """
+
+    return {
+        assg_share: pipe(
+            clusters_silh_scores.query(f"assign_strategy=='{assg_share}'"),
+            lambda df: df.loc[df["score"] > df["score"].quantile(q)]["cluster"],
+            set,
+        )
+        for assg_share in clusters_silh_scores["assign_strategy"].unique()
+    }
