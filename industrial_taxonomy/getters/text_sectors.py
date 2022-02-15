2147f6e1093dc3d5964b075d49bcb7142ed4bb44
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-02-15 16:52:17 +0000

    added filtered option to text getter

--- industrial_taxonomy/getters/text_sectors.py
+++ industrial_taxonomy/getters/text_sectors.py
@@ -13,11 +13,13 @@ glass_id = str
 
 
 def text_sectors(
+    filtered: bool = True,
     run: Optional[Run] = None,
 ) -> Dict[clustering_param, List[Tuple[sector_id, glass_id]]]:
     """Gets text sector assignments from the topic modelling
 
     Args:
+        filtered: filters low silhouette score clusters
         run: what ClusterGlass flow run to use.
             Defaults to the latest production run
 
@@ -29,7 +31,22 @@ def text_sectors(
 
     """
     run = run or get_run("ClusterGlass")
-    return run.data.clusters
+
+    if filtered is True:
+        clusters = run.data.clusters
+        filtered_clusters = get_run("FilterClusters").data.text_sectors_filtered
+
+        return {
+            assign_share: [
+                text_s
+                for text_s in sectors
+                if text_s[0] in filtered_clusters[assign_share]
+            ]
+            for assign_share, sectors in clusters.items()
+        }
+
+    else:
+        return run.data.clusters
 
 
 def topsbm_models(run: Optional[Run] = None) -> Dict[sector_id, sbmtm]:
