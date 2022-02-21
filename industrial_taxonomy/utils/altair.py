70311a0a6a49b69b263440a3360812eb11279260
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-02-21 21:46:24 +0000

    script with altair plotting utils

--- industrial_taxonomy/utils/altair.py
+++ industrial_taxonomy/utils/altair.py
@@ -1,8 +1,11 @@
 # Scripts to save altair charts
 
+import json
 import os
+import altair as alt
 from altair import Chart
 from altair_saver import save
+from geopandas import GeoDataFrame
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -49,9 +52,61 @@ def altair_text_resize(chart: Chart, sizes: tuple = (12, 14)) -> Chart:
         chart: chart to resize
         sizes: label size and title size
 
+    Returns:
+        An altair chart
     """
 
     ch = chart.configure_axis(
         labelFontSize=sizes[0], titleFontSize=sizes[1]
     ).configure_legend(labelFontSize=sizes[0], titleFontSize=sizes[1])
     return ch
+
+
+def choro_plot(
+    geo_df: GeoDataFrame,
+    count_var: str,
+    count_var_name: str,
+    region_name: str = "region",
+    scheme: str = "spectral",
+    scale_type: str = "linear",
+) -> Chart:
+    """Plot an altair choropleth
+
+    Args:
+        geo_df: geodf
+        count_var: name of the variable we are plotting
+        count_var_name: clean name for the count variable
+        region_name is the name of the region variable
+        scheme: is the colour scheme. Defaults to spectral
+        scale_type: is the type of scale we are using. Defaults to log
+
+    Returns:
+        An altair chart
+
+    """
+
+    base_map = (  # Base chart with outlines
+        alt.Chart(alt.Data(values=json.loads(geo_df.to_json())["features"]))
+        .project(type="mercator")
+        .mark_geoshape(filled=False, stroke="gray")
+    )
+
+    choropleth = (  # Filled polygons and tooltip
+        base_map.transform_calculate(region=f"datum.properties.{region_name}")
+        .mark_geoshape(filled=True, stroke="darkgrey", strokeWidth=0.2)
+        .encode(
+            size=f"properties.{count_var}:Q",
+            color=alt.Color(
+                f"properties.{count_var}:Q",
+                title=count_var_name,
+                scale=alt.Scale(scheme="Spectral", type=scale_type),
+                sort="descending",
+            ),
+            tooltip=[
+                "region:N",
+                alt.Tooltip(f"properties.{count_var}:Q", format="1.2f"),
+            ],
+        )
+    )
+
+    return base_map + choropleth
